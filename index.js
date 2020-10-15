a97b145e568b665112183c6046a1918b2ad4d4cf
MammothOG <felix.capuano@gmail.com>
2020-10-15 13:19:23 +0200

    establish communication

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const express = require('express');
 const app = express();
 
 PORT = 3000;
+var requestCount = 0;
 
 app.listen(PORT, () => console.log("listening at " + PORT));
-app.use(express.static('public'));
+
+// use html page from public folder
+//app.use(express.static('public'));
+app.use(express.json())
+
+app.post("/api", (req, rep) => {
+    console.log("New post request")
+    console.log(req.body);
+    console.log(requestCount);
+    requestCount = requestCount + 1;
+})
