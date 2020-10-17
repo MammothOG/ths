7927b7b4e327af4601aa5f7bd02863955b8ed17d
MammothOG <felix.capuano@gmail.com>
2020-10-17 21:47:22 +0200

    avancement

--- index.js
+++ index.js
@@ -1,70 +1,76 @@
-const { exec, spawn } = require("child_process")
-
 const express = require('express');
-    const app = express();
+const app = express();
 
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-// setup display
-exec("export DISPLAY=:0", (error, stdout, stderr) => {
-    console.log("exec: export DISPLAY=:0");
-    if (error) {
-        console.log(`error: ${error.message}`);
-        return;
-    }
-    if (stderr) {
-        console.log(`stderr: ${stderr}`);
-        return;
-    }
-});
+app.listen(PORT, () => console.log(`Web server running [port = ${PORT}]`));
 
-app.listen(port, () => console.log("Web server running [port = " + port + "]"));
-
-// use html page from public folder
-//app.use(express.static('public'));
 app.use(express.json());
+app.use(express.static('public'));
+//app.use((req, res) => {
+//    res.status(404).send('Unknown Request');
+//})
+
 
 app.post("/api", (req, res) => {
     console.log("Receive post request");
 
-    const data = req.body;
-    switch (data.content.service) {
-        case "youtube":
-            execute(playYoutubeCmd(data));
-            break;
-        default:
-            console.warn("Service not suported => " + data.content.service);
-            break;
-    }
-
     res.sendStatus(200);
 });
 
-const playYoutubeCmd = (ytData) => {
-    return {
-        name: "Play Youtube video",
-        command: "cvlc",
-        args: ["-f", ytData.content.path],
-        status: ytData.params.status,
-    }
-}
 
-const execute = (cmd) => {
-    const shell = spawn(cmd.command, cmd.args);
+//const { exec, spawn } = require("child_process");
+    //const data = req.body;
 
-    shell.stdout.on("data", data => {
-        console.log(`stdout: ${data}`);
-    });
+    //switch (data.content.service) {
+    //    case "youtube":
+    //        console.log("receive youtube question")
+    //        player.play();
+    //        //execute(playYoutubeCmd(data));
+    //        break;
+    //    default:
+    //        console.warn("Service not suported => " + data.content.service);
+    //        break;
+    //}
 
-    shell.stderr.on("data", data => {
-        console.log(`stderr: ${data}`);
-    });
-
-    shell.on('error', (error) => {
-        console.log(`error: ${error.message}`);
-    });
-
-    shell.on("close", code => {
-        console.log(`child process exited with code ${code}`);
-    });
-}
+// setup display
+//exec("export DISPLAY=:0", (error, stdout, stderr) => {
+//    console.log("exec: export DISPLAY=:0");
+//    if (error) {
+//        console.log(`error: ${error.message}`);
+//        return;
+//    }
+//    if (stderr) {
+//        console.log(`stderr: ${stderr}`);
+//        return;
+//    }
+//});
+//const playYoutubeCmd = (ytData) => {
+//    // controle vlc --extraintf="rc"
+//    return {
+//        name: "Play Youtube video",
+//        command: "cvlc",
+//        args: ["-f", ytData.content.path],
+//        status: ytData.params.status,
+//    }
+//}
+//
+//const execute = (cmd) => {
+//    const shell = spawn(cmd.command, cmd.args);
+//
+//    shell.stdout.on("data", data => {
+//        console.log(`stdout: ${data}`);
+//    });
+//
+//    shell.stderr.on("data", data => {
+//        console.log(`stderr: ${data}`);
+//    });
+//
+//    shell.on('error', (error) => {
+//        console.log(`error: ${error.message}`);
+//    });
+//
+//    shell.on("close", code => {
+//        console.log(`child process exited with code ${code}`);
+//    });
+//}
