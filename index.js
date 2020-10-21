294559754230e3a7ccc2b5ca799becf233f82b79
MammothOG <felix.capuano@gmail.com>
2020-10-21 16:07:28 +0200

    gogogo

--- index.js
+++ index.js
@@ -28,18 +28,7 @@ io.on('connection', (socket) => {
   app.post("/remote", postHandler_(remoteHandler));
 });
 
-const remoteHandler = (req, res) => {
-  console.log("Receive post request");
-  const data = req.body;
 
-  if (isDataFormatCorrect(data)) {
-    io.emit('request', "new data");
-    res.status(200).send(data);
-  }
-  else {
-    res.status(400).send(data);
-  }
-}
 
 
 
