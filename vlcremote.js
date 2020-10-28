82c7beddff1d64251c573c92ad043ed7e4199e12
MammothOG <felix.capuano@gmail.com>
2020-10-28 14:03:13 +0100

    setup vlc

--- vlcremote.js
+++ vlcremote.js
@@ -1,12 +1,11 @@
 const VLC = require("vlc-client");
-const vlc = new VLC.Client({
-    ip: "localhost",
-    port: 8080,
-    username: "toto", // optional
-    password: "toto"
-});
+const { exec, spawn } = require("child_process");
+const { createWriteStream } = require("fs");
+const path = require("path");
 
-switch (platform()) {
+
+
+switch (process.platform) {
     case "darwin":
         cmd = "/Applications/VLC.app/Contents/MacOS/VLC";
         intf = "macosx";
@@ -16,22 +15,65 @@ switch (platform()) {
         intf = "qt"
         break;
     default:
-        throw new Error(`Platform '${platform()}' is not supported for testing`);
+        throw new Error(`Platform '${process.platform}' is not supported for testing`);
 }
 
 
 const args = [
-	"-I", intf,
-	"--extraintf", "http",
-	"--http-port", "8080",
-	"--http-host", "localhost",
-	"--http-password","1234",
+    "-I", intf,
+    "--extraintf", "http",
+    "--http-port", "8080",
+    "--http-host", "localhost",
+    "--http-password", "1234",
 ];
 
-console.log(vlc.status());
+exec("export DISPLAY=:0", (error, stdout, stderr) => {
+    console.log("exec: export DISPLAY=:0");
+});
 
-const vlcTranslator = (request) => {
-    console.log(request);
+const spawnVlc = async () => {
+    const vlcProcess = spawn(cmd, args, {
+        stdio: "pipe"
+    });
+
+    console.log(path.join(__dirname, "../vlc.log"))
+    const output = createWriteStream(path.join(__dirname, "vlc.log"));
+    vlcProcess.stderr.pipe(output);
+    vlcProcess.stdout.pipe(output);
+    vlcProcess.on("exit", () => {
+        output.close();
+    })
+
+    console.log([vlcProcess.spawnfile, ...vlcProcess.spawnargs].join(" "));
+
+    return new Promise((res, rej) => {
+        const listener = (isError, error) => {
+            vlcProcess.stderr.removeListener("data", listener);
+            vlcProcess.stdout.removeListener("data", listener);
+            vlcProcess.removeListener("error", listener);
+
+            if (isError) {
+                rej(error);
+                vlcProcess.kill();
+            } else {
+                setTimeout(() => {
+                    res(vlcProcess);
+                }, 2000);
+            }
+        }
+
+        vlcProcess.stdout.on("data", listener.bind(listener, false));
+        vlcProcess.stderr.on("data", listener.bind(listener, false));
+        vlcProcess.on("error", listener.bind(listener, true));
+    })
 }
 
-exports.vlcTranslator = vlcTranslator;
+(async () => {
+    let vlcProcess = await spawnVlc();
+    let vlc = new VLC.Client({
+        ip: "localhost",
+        port: 8080,
+        password: "1234"
+    });
+    await vlc.playFile('vid/sample.mp4');
+})();
